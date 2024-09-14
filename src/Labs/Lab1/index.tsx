81d9408c77f933aaf8d72dc700f5b732225d8cfa
Weiqing Gao <wkinggao@gmail.com>
2024-09-14 16:37:25 -0400

    complete 2.8

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -3,6 +3,7 @@ export default function Lab1() {
         <div id="wd-lab1">
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
+
             <div id="wd-h-tag">
                 <h4>Heading Tags</h4>
                 Text documents are often broken up into several sections and 
@@ -50,6 +51,53 @@ the right.
 This is the third paragraph. Wrap each paragraph with the paragraph
 tag to tell browsers to render the gaps.                    
                 </p>
+            </div>
+
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                <ol id="wd-pancakes">
+                    <li>Mix dry ingredient.</li>
+                    <li>Add wet ingredients.</li>
+                    <li>Stir to combine.</li>
+                    <li>Heat a skillet or griddle.</li>
+                    <li>Pour batter onto the skillet.</li>
+                    <li>Cook until bubbly on top.</li>
+                    <li>Flip and cook the other side.</li>
+                    <li>Serve and enjoy!</li>
+                </ol> 
+                My favorite recipe:
+                <ol id="wd-my-favorite-recipe">
+                    <li>Cut the ribs into 4cm pieces.</li>
+                    <li>Place them into a pot of cold water.</li>
+                    <li>Add green onions, ginger, and cooking wine.</li>
+                    <li>Heat to boil, wait for 3 minutes.</li>
+                    <li>Turn off the heat and take the ribs out of the pot.</li>
+                    <li>Crush the rock sugar and place into a clean pot.</li>
+                    <li>Heat over low heat until the sugar melts and starts bubbling.</li>
+                    <li>Add the ribs and stir-fry 30 seconds.</li>
+                    <li>Add enough boiling water to cover all the ribs.</li>
+                    <li>Add soy sauce, vinegar, granulated sugar, oyster sauce, bay leaves, and star anise.</li>
+                    <li>Cover with a lid and simmer over low heat for 30 minutes.</li>
+                    <li>Add salt, then increase the heat to high and reduce the sauce to desired consistency.</li>
+                </ol>
+
+                <h5>Unordered List Tag</h5>
+                My favorite books (in no particular order)
+                <ul id="wd-my-books">
+                    <li>Dune</li>
+                    <li>Lord of the Rings</li>
+                    <li>Ender's Game</li>
+                    <li>Red Mars</li>
+                    <li>The Forever Var</li>
+                </ul>
+                Your favorite books (in no particular order)
+                <ul id="wd-your-books">
+                    <li>The Think Tank</li>
+                    <li>Silent Spring</li>
+                    <li>Romance of the Three Kingdoms</li>
+                </ul>
             </div>
         </div>
     );
