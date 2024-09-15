2c1d86b6a5429ca965126e59d1d5bfcbfd6de6f2
Weiqing Gao <wkinggao@gmail.com>
2024-09-14 22:18:38 -0400

    complete 2.11

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -196,7 +196,114 @@ tag to tell browsers to render the gaps.
                 <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
             </div>
 
-            
+            <div id="wd-forms">
+                <h4>Form Elements</h4>
+                <form id="wd-text-fileds">
+                    <h5>Text Fields</h5>
+                    <label htmlFor="wd-text-fields-username">Username:</label>
+                    <input id="wd-text-fields-username" placeholder="jdoe" /><br />
+                    <label htmlFor="wd-text-fields-password">Password:</label>
+                    <input id="wd-text-fields-password" value="123@#$asd" /><br />
+                    <label htmlFor="wd-text-fields-first-name">First name:</label>
+                    <input id="wd-text-fields-first-name" title="John" /> <br />
+                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                    <input id="wd-text-fields-last-name" placeholder="Doe" 
+                    value="Wonderland" title="The last name" />
+                </form>
+            </div>
+
+            <h5>Text boxes</h5>
+            <label>Biography:</label><br/>
+            <textarea id="wd-textarea" cols={30} rows={10}>"But I must explain to you how all 
+                this mistaken idea of denouncing pleasure and praising pain was born 
+                and I will give you a complete account of the system, and expound the 
+                actual teachings of the great explorer of the truth, the 
+                master-builder of human happiness. No one rejects, dislikes, or avoids 
+                pleasure itself, because it is pleasure, but because those who do not 
+                know how to pursue pleasure rationally encounter consequences that are 
+                extremely painful. Nor again is there anyone who loves or pursues or 
+                desires to obtain pain of itself, because it is pain, but because 
+                occasionally circumstances occur in which toil and pain can procure him 
+                some great pleasure. To take a trivial example, which of us ever 
+                undertakes laborious physical exercise, except to obtain some advantage 
+                from it? But who has any right to find fault with a man who chooses to 
+                enjoy a pleasure that has no annoying consequences, or one who avoids a 
+                pain that produces no resultant pleasure?"
+            </textarea>
+
+            <h5 id="wd-buttons">Buttons</h5>
+            <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
+            Hello World!
+            </button>  
+
+            <h5>File upload</h5>
+            <input id="wd-upload" type="file"/>
+
+            <h5 id="wd-radio-buttons">Radio buttons</h5>
+            <label>Favorite movie genre:</label><br />
+            <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+            <label htmlFor="wd-radio-comedy">Comedy</label><br />
+            <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+            <label htmlFor="wd-radio-drama">Drama</label><br />
+            <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+            <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+            <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+            <label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+            <h5 id="wd-checkboxes">Checkboxes</h5>
+            <label>Favorite movie genre:</label><br/>
+            <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+            <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+            <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+            <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+            <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+            <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+            <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+            <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+            <h4 id="wd-dropdowns">Dropdowns</h4>
+            <h5>Select one</h5>
+            <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+            <select id="wd-select-one-genre">
+            <option value="COMEDY">Comedy</option>
+            <option value="DRAMA">Drama</option>
+            <option selected value="SCIFI">
+                Science Fiction</option>
+            <option value="FANTASY">Fantasy</option>
+            </select>
+            <h5>Select many</h5>
+            <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+            <select id="wd-select-many-genre" multiple>
+            <option selected value="COMEDY">Comedy</option>
+            <option value="DRAMA">Drama</option>
+            <option selected value="SCIFI">
+                Science Fiction</option>
+            <option value="FANTASY">Fantasy</option>
+            </select>
+
+            <h4>Other HTML field types</h4>
+            <label htmlFor="wd-text-fields-email"> Email: </label>
+            <input type="email"
+                placeholder="jdoe@somewhere.com"
+                id="wd-text-fields-email"/><br/>
+
+            <label htmlFor="wd-text-fields-salary-start"> Starting salary:
+            </label>
+            <input type="number"
+                id="wd-text-fields-salary-start"
+                placeholder="1000"
+                value="100000"/><br/>
+
+            <label htmlFor="wd-text-fields-rating"> Rating: </label>
+            <input type="range" id="wd-text-fields-rating"
+                placeholder="Doe"
+                max="5"
+                value="4"/><br/>
+                
+            <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+            <input type="date"
+                id="wd-text-fields-dob"
+                value="2000-01-21"/><br/>
         </div>
     );
 }
