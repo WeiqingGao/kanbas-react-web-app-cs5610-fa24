b9f754240ac15e4e882f59f051a434adcab74e1c
Weiqing Gao <wkinggao@gmail.com>
2024-10-02 20:50:05 -0400

    complete 4.8

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,6 +4,7 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import PeopleTable from "./People/Table";
 import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
@@ -25,7 +26,7 @@ export default function Courses() {
                         <Route path="Modules" element={<Modules />} />
                         <Route path="Assignments" element={<Assignments/>} />
                         <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-                        <Route path="People" element={<h2>People</h2>} />
+                        <Route path="People" element={<PeopleTable />} />                    
                     </Routes>
                 </div>
             </div>
