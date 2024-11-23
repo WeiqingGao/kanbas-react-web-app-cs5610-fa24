233a5222df7b27df0be8fe6c1c003b6f6410b320
Weiqing Gao <wkinggao@gmail.com>
2024-11-22 20:36:56 -0500

    Fix bugs coursed by files move

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,8 +1,7 @@
-import courses from "./courses.js";
-import modules from "./modules.js";
-import assignments from "./assignments.js";
-// import grades from "./grades.js";
-import users from "./users.js";
-import enrollments from "./enrollments.js";
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+import enrollments from "./enrollments.json";
 
 export { courses, modules, assignments, users, enrollments };
